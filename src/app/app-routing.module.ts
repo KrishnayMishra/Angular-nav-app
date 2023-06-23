8170f832c96457daff1c9203ac4ae2c0f98eebbe
KrishnayMishra <manyamishra292@gmail.com>
2023-06-23 19:01:07 +0530

    3rd

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddComponent } from './add/add.component';
+import { ListComponent } from './list/list.component';
+import { SearchComponent } from './search/search.component';
+import { SortComponent } from './sort/sort.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'add', component: AddComponent}, 
+  {path: 'list', component: ListComponent}, 
+  {path:'find', component:SearchComponent},
+  {path:'sort', component: SortComponent},
+  {path:'**', redirectTo: '/add', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddComponent } from './add/add.component';
+import { ListComponent } from './list/list.component';
+import { SearchComponent } from './search/search.component';
+import { SortComponent } from './sort/sort.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddComponent,
+    ListComponent,
+    SearchComponent,
+    SortComponent
   ],
   imports: [
     BrowserModule,
